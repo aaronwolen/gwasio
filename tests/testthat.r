011e2ec092f91f0ef7be85f2eea0611a9950148e
Aaron Wolen <aaron@wolen.com>
2016-09-21 11:03:06 -0400

    First pass at tests for label variants

---
+++ tests/testthat.r
@@ -0,0 +1,4 @@
+library(testthat)
+library(gwasio)
+
+test_check("gwasio")
---
+++ tests/testthat/test-column-variants.r
@@ -0,0 +1,28 @@
+context("Column label variants")
+
+test_that("canonical labels recognized", {
+  file    <- "tables/columns-variant1-canonical.txt"
+  labels  <- names(.gwas_patterns)
+  table   <- read_gwas(file, verbose = FALSE)
+  expect_equal(colnames(table), labels)
+})
+
+test_that("abbreviated labels recognized", {
+  file    <- "tables/columns-variant2-abbreviated.txt"
+  labels  <- names(.gwas_patterns)
+  table   <- read_gwas(file, verbose = FALSE)
+  expect_equal(colnames(table), labels)
+})
+
+test_that("variant 3 recognized", {
+  file    <- "tables/columns-variant3.txt"
+  labels  <-
+    c("marker",
+      "position",
+      "a1",
+      "a2",
+      "frequency",
+      "pvalue")
+  table   <- read_gwas(file, verbose = FALSE)
+  expect_equal(colnames(table), labels)
+})
